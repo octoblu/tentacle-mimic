fc9b4d8c0cbc52fcac15556863035297209a64d8
Roy van de Water <roy.vwater@gmail.com>
2015-09-09 10:09:36 -0700

    It compiles

--- mimic.cpp
+++ mimic.cpp
@@ -7,9 +7,33 @@
 static const char uuid[]  = "ec1a3aa1-e5a0-4134-ab49-1bdac08b8616";
 static const char token[] = "09fee3756f02154bb388b81b4c43cca263015085";
 
+void connectToServer();
+
 int main (int argc, char *argv[])
 {
-  StdioStream myStream;
-  std::cout << "Hello World " << myStream.available();
+  StdioStream stdioStream;
+  TentacleArduino tentacle;
+  Pseudopod pseudopod(stdioStream, stdioStream, tentacle);
+
+  connectToServer();
+
+  std::cout << "Hello World " << stdioStream.available();
   return 0;
+}
+
+void connectToServer() {
+  // int connectionAttempts = 0;
+  // Serial.println(F("Connecting to the server."));
+  // Serial.flush();
+  //
+  // while(!conn.connect(server, port)) {
+  //   Serial.println(F("Can't connect to the server."));
+  //   Serial.flush();
+  //   conn.stop();
+  //   connectionAttempts++;
+  // }
+  //
+  // size_t authSize = pseudopod.authenticate(uuid, token);
+  // Serial.print(authSize);
+  // Serial.println(F(" bytes written for authentication"));
 }
--- mimic.h
+++ mimic.h
@@ -1,3 +1,9 @@
 #include "tentacle.h"
+#include "tentacle-arduino.h"
 #include "tentacle-pseudopod.h"
 #include "stdio-stream.h"
+
+#include "boost/archive/binary_iarchive.hpp"
+#include "boost/archive/binary_oarchive.hpp"
+#include "boost/lexical_cast.hpp"
+#include "boost/asio.hpp"
